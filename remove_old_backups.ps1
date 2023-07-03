9f10adfb4151f5892f539fc09113b1ec67ce1f7b
Oleh Snieosikov <Oleh.Snieosikov@epam.com>
2023-07-03 10:56:01 +0300

    Create remove_old_backups.ps1

---
+++ remove_old_backups.ps1
@@ -0,0 +1,7 @@
+﻿# Delete all Files in older than 30 day(s)
+$Path = "d:\Backup"
+$Daysback = "-30"
+ 
+$CurrentDate = Get-Date
+$DatetoDelete = $CurrentDate.AddDays($Daysback)
+Get-ChildItem $Path -Recurse  | Where-Object { $_.LastWriteTime -lt $DatetoDelete } | Remove-Item
